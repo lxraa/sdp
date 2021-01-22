dd2746d94fed97325f252c2f1258a80645691564
liuxiaorui <liuxiaorui@bestpay.com.cn>
2021-01-22 20:55:15 +0800

    目前存在两个问题：1、TCP流怎么解析成应用层协议；2、如何区分https和http

--- src/main/java/com/lxraa/proxy/netty/socks5/client/Client2DestHandler.java
+++ src/main/java/com/lxraa/proxy/netty/socks5/client/Client2DestHandler.java
@@ -1,11 +1,15 @@
 package com.lxraa.proxy.netty.socks5.client;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+import java.nio.charset.Charset;
 
 /**
- * 将目标服务器信息转发给客户端
+ * 将客户端信息转发给目标服务器
  *
  * @author
  *
@@ -20,6 +24,11 @@ public class Client2DestHandler extends ChannelInboundHandlerAdapter {
 
         @Override
         public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf buffer = (ByteBuf) msg;
+            Integer len = buffer.readableBytes();
+            byte[] b = new byte[len];
+            buffer.getBytes(buffer.readerIndex(),b);
+            System.out.println(new String(b, Charset.forName("utf-8")));
             destChannelFuture.channel().writeAndFlush(msg);
         }
 
